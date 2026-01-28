577dbf60d35be4e476bac01709b8db49ca3f0029
Mikael <mikael251101@gmail.com>
2026-01-28 13:03:57 +0700

    Day 12: PDF generated financial report

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from interface.input_handler import load_businesses_from_csv
 from engine.analyzer import analyze_business
 from reports.printer import print_report
+from reports.pdf_generator import generate_pdf
 
 
 def main():
     businesses = load_businesses_from_csv("data/business_data.csv")
 
     for i, business in enumerate(businesses, start=1):
-        print(f"\n=== BUSINESS #{i} ===")
         result = analyze_business(business)
+
         print_report(business, result)
+        generate_pdf(business, result, i)
 
 
 if __name__ == "__main__":
